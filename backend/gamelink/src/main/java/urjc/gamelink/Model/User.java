aa653731d70e5f127268ff8561c8b6ec8af5d973
andrecupa <andrecupa@gmail.com>
2022-02-22 21:57:34 +0100

    model

---
+++ backend/gamelink/src/main/java/urjc/gamelink/Model/User.java
@@ -0,0 +1,122 @@
+package urjc.gamelink.Model;
+
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "UserTable")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String name;
+	
+	private String lastName;
+
+	private String email;
+
+	private String encodedPassword;
+
+	private boolean image;
+
+	@Lob
+	private Blob imageFile;
+
+	@ManyToMany
+ 	private List<News> readNotices;
+
+	@ManyToMany
+ 	private List<Videogame> purchaseVideogames;
+
+	@ElementCollection(fetch = FetchType.EAGER)
+	private List<String> roles;
+
+	public User() {
+	}
+
+	public User(String name, String encodedPassword, String... roles) {
+		this.name = name;
+		this.encodedPassword = encodedPassword;
+		this.roles = List.of(roles);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Blob getImageFile() {
+		return imageFile;
+	}
+
+	public void setImageFile(Blob image) {
+		this.imageFile = imageFile;
+	}
+
+	public boolean getImage(){
+		return this.image;
+	}
+
+	public void setImage(boolean image){
+		this.image = image;
+	}
+
+	public String getEncodedPassword() {
+		return encodedPassword;
+	}
+
+	public void setEncodedPassword(String encodedPassword) {
+		this.encodedPassword = encodedPassword;
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+
+	public List<News> getReadNotices() {
+		return readNotices;
+	}
+
+	public void setReadNotices(List<News>readNotices) {
+		this.readNotices = readNotices;
+	}
+
+	public List<Videogame> getPurchaseVideogames() {
+		return purchaseVideogames;
+	}
+
+	public void setPurchaseVideogames(List<Videogame> purchaseVideogames) {
+		this.purchaseVideogames = purchaseVideogames;
+	}
+
+}
