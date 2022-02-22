459cff07bc03944e4ddf1e2dbbc52fa1025d8104
juanl <juanluis123.rico@gmail.com>
2022-02-22 10:45:21 +0100

    GetMapping completo y asignacicion WebSecurityConfig para los distintos usuarios

---
+++ backend/gamelink/src/main/java/urjc/gamelink/WebSecurityConfig.java
@@ -0,0 +1,67 @@
+package urjc.gamelink;
+
+
+import java.security.SecureRandom;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /*@Autowired
+	RepositoryUserDetailsService userDetailsService;*/
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(10, new SecureRandom());
+	}
+
+	/*@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}*/
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	
+    	// Public pages
+        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests().antMatchers("/about").permitAll();
+        http.authorizeRequests().antMatchers("/videogameStatistics").permitAll();
+        http.authorizeRequests().antMatchers("/errorMessage").permitAll();
+        http.authorizeRequests().antMatchers("/home").permitAll();
+        http.authorizeRequests().antMatchers("/news").permitAll();
+        http.authorizeRequests().antMatchers("/signin").permitAll();
+        http.authorizeRequests().antMatchers("/showNews").permitAll();
+        http.authorizeRequests().antMatchers("/showVideogame").permitAll();
+        http.authorizeRequests().antMatchers("/videogame").permitAll();
+        http.authorizeRequests().antMatchers("/").permitAll();
+
+        // Private pages
+        http.authorizeRequests().antMatchers("/admin").hasAnyRole("ADMIN");
+        http.authorizeRequests().antMatchers("/paymentConfirmation").hasAnyRole("USER");
+        http.authorizeRequests().antMatchers("/login").hasAnyRole("USER");
+        http.authorizeRequests().antMatchers("/userProfile").hasAnyRole("USER");
+
+        // Login form
+        http.formLogin().loginPage("/login");
+        http.formLogin().usernameParameter("username");
+        http.formLogin().passwordParameter("password");
+        http.formLogin().defaultSuccessUrl("/");
+        http.formLogin().failureUrl("/loginerror");
+
+        // Logout
+        http.logout().logoutUrl("/logout");
+        http.logout().logoutSuccessUrl("/");
+    }
+    
+}
