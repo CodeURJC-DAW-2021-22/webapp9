de32e44508ede7bc24c1358de88172ee7e3e9155
JhostinD <jhostin2906@gmail.com>
2022-02-23 21:28:48 +0100

    arreglado main sin doble carpeta gamelink

---
+++ backend/gamelink/src/main/java/urjc/gamelink/Controllers/GamelinkController.java
@@ -0,0 +1,118 @@
+package urjc.gamelink.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import urjc.gamelink.Service.NewsService;
+import urjc.gamelink.Service.VideogameService;
+
+@Controller
+public class GamelinkController {
+
+    @Autowired
+    private NewsService ns;
+
+    @Autowired
+    private VideogameService vs;
+    
+
+    @GetMapping("/")
+    public String home(Model model){
+
+        model.addAttribute("new", ns.findAll());
+        
+        return "home";
+
+    }
+
+
+    @GetMapping("/about")
+    public String about(Model model){
+        
+        return "about";
+
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model){
+        
+        return "admin";
+
+    }
+
+    @GetMapping("/paymentConfirmation")
+    public String paymentConfirmation(Model model){
+        
+        return "paymentConfirmation";
+
+    }
+
+    @GetMapping("/videogameStatistics")
+    public String videogameStatistics(Model model){
+        
+        return "videogameStatistics";
+
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        
+        return "login";
+
+    }
+
+    @GetMapping("/errorMessage")
+    public String errorMessage(Model model){
+        
+        return "errorMessage";
+
+    }
+
+    @GetMapping("/news")
+    public String news(Model model){
+        
+        return "news";
+
+    }
+
+    @GetMapping("/userProfile")
+    public String userProfile(Model model){
+        
+        return "userProfile";
+
+    }
+
+    @GetMapping("/signin")
+    public String signin(Model model){
+        
+        return "signin";
+
+    }
+
+    @GetMapping("/showNews")
+    public String showNews(Model model){
+        
+        return "showNews";
+
+    }
+
+    @GetMapping("/showVideogame")
+    public String showVideogame(Model model){
+        
+        return "showVideogame";
+
+    }
+
+    @GetMapping("/videogame")
+    public String videogame(Model model) {
+
+        model.addAttribute("game_name" , "Battlefield 2042");
+
+        return "videogame";
+    }
+
+
+
+}
---
+++ backend/gamelink/src/main/java/urjc/gamelink/Model/News.java
@@ -0,0 +1,130 @@
+package urjc.gamelink.Model;
+
+import java.sql.Blob;
+import java.util.List;
+
+import javax.persistence.Column;
+//import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+//import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+
+@Entity(name = "tbl_news")
+public class News {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private boolean image;
+
+    @Lob
+    private Blob imageFile;
+
+    private String author;
+
+    private String date;
+
+    private String readTime;
+
+    private String badge;
+
+
+    @ManyToMany
+     private List<Videogame> videogamesRelated;
+
+     public News() {
+    }
+
+    public News(String title,String description, String author, String date ,String readtime, String badge) {
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.date = date;
+        this.readTime = readtime;
+        this.badge = badge;
+    }
+
+    
+    public String getReadTime() {
+        return readTime;
+    }
+
+    public void setReadTime(String readTime) {
+        this.readTime = readTime;
+    }
+
+    public String getBadge() {
+        return badge;
+    }
+
+    public void setBadge(String badge) {
+        this.badge = badge;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Blob getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(Blob imageFile) {
+        this.imageFile = imageFile;
+    }
+
+    public boolean getImage(){
+        return this.image;
+    }
+
+    public void setImage(boolean image){
+        this.image = image;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public List<Videogame> getVideogamesRelated() {
+        return videogamesRelated;
+    }
+
+    public void setVideogamesRelated(List<Videogame> videogamesRelated) {
+        this.videogamesRelated = videogamesRelated;
+    }
+
+}
