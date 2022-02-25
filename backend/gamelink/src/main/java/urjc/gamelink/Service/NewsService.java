24f347bf6bd576a1a807f6bb8050414afbfb1a0f
ultramani <71970337+ultramani@users.noreply.github.com>
2022-02-25 15:49:34 +0100

    Fixed image not loading 500 response

--- backend/gamelink/src/main/java/urjc/gamelink/Service/NewsService.java
+++ backend/gamelink/src/main/java/urjc/gamelink/Service/NewsService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import urjc.gamelink.Model.News;
@@ -34,4 +37,5 @@ public class NewsService {
 	public void delete(long id) {
 		repository.deleteById(id);
 	}
+
 }
