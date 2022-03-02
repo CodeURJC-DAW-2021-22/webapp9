27c926a175f6b193336db5de85b9acbf7492d1bc
ultramani <71970337+ultramani@users.noreply.github.com>
2022-03-02 22:12:32 +0100

    Edit new implemented

--- backend/gamelink/src/main/java/urjc/gamelink/Model/PurchaseCode.java
+++ backend/gamelink/src/main/java/urjc/gamelink/Model/PurchaseCode.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+
 import org.hibernate.annotations.OnDelete;
 
 @Entity(name="tbl_purchase_code")
