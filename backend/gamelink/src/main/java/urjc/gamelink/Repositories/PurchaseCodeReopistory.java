91f3c1df5d18a4c3e4ad58c55e0e162f13ca3804
JLRR2019 <juanluis123.rico@gmail.com>
2022-03-13 22:44:41 +0100

    hard reset

--- backend/gamelink/src/main/java/urjc/gamelink/Repositories/PurchaseCodeReopistory.java
+++
@@ -1,9 +0,0 @@
-package urjc.gamelink.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import urjc.gamelink.Model.PurchaseCode;
-
-public interface PurchaseCodeReopistory extends JpaRepository<PurchaseCode, Long> {
-
-}
