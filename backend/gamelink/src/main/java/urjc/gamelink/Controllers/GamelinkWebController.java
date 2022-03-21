7363714f3115a2c4344abfd4af623ae74a5cff8f
JhostinD <jhostin2906@gmail.com>
2022-03-22 00:12:23 +0100

    primera version controlador por entidad

--- backend/gamelink/src/main/java/urjc/gamelink/Controllers/GamelinkWebController.java
+++ backend/gamelink/src/main/java/urjc/gamelink/Controllers/GamelinkWebController.java
@@ -1,9 +1,7 @@
 package urjc.gamelink.Controllers;
 
-import java.io.IOException;
 import java.security.Principal;
 import java.sql.Date;
-import java.sql.SQLException;
 //import java.text.SimpleDateFormat;
 import java.util.Optional;
 
@@ -12,14 +10,9 @@ import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,7 +21,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 
 import urjc.gamelink.Model.News;
@@ -146,46 +138,6 @@ public class GamelinkWebController {
 
     }
 
-
-    @GetMapping("/userProfile")
-    public String userProfile(Model model, HttpServletRequest request){
-
-        String name = request.getUserPrincipal().getName();
-        Usero user = us.findByName(name).orElseThrow();
-        model.addAttribute("username", user.getName());
-        model.addAttribute("nick", user.getNick());
-        model.addAttribute("encodedPassword", user.getEncodedPassword());
-        model.addAttribute("lastName", user.getLastName());
-        model.addAttribute("creditCard", user.getCreditCard());
-        model.addAttribute("email", user.getEmail());
-        model.addAttribute("Videogame", user.getPurchaseVideogames());
-        model.addAttribute("id", user.getId());
-
-        return "userProfile";
-
-    }
-    
-    @PostMapping("/userProfile")
-    public String userProfile(Model model, HttpServletRequest request, @RequestParam String nick, @RequestParam String email,
-                                @RequestParam String creditCard, MultipartFile imageField) throws IOException{
-                                    
-        String useroName = request.getUserPrincipal().getName();
-        Usero user = us.findByName(useroName).orElseThrow();  
-        user.setNick(nick);
-        user.setEmail(email);                            
-        user.setCreditCard(creditCard);
-
-        if(!imageField.isEmpty()){
-            user.setImageFile(BlobProxy.generateProxy(imageField.getInputStream(), imageField.getSize()));
-            user.setImage(true);
-        }  
-
-        us.save(user);
-
-
-        return "redirect:/userProfile";                            
-    }
-
     @GetMapping("/signin")
     public String signin(Model model){
 
@@ -215,48 +167,5 @@ public class GamelinkWebController {
         return "home";
 
     }
-
-    @GetMapping("/profile/{id}/image")
-	public ResponseEntity<Object> downloadImageProfile(@PathVariable long id) throws SQLException {
-
-		Optional<Usero> user = us.findById(id);
-		if (user.isPresent() && user.get().getImageFile() != null) {
-
-			Resource file = new InputStreamResource(user.get().getImageFile().getBinaryStream());
-
-			return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/PNG")
-					.contentLength(user.get().getImageFile().length()).body(file);
-
-		} else {
-			return ResponseEntity.notFound().build();
-		}
-	}
-
-    @GetMapping("/usero/{id}/image")
-	public ResponseEntity<Object> downloadImageUsero(@PathVariable long id) throws SQLException {
-
-		Optional<Videogame> videogame = vs.findById(id);
-		if (videogame.isPresent() && videogame.get().getImageFile() != null) {
-
-			Resource file = new InputStreamResource(videogame.get().getImageFile().getBinaryStream());
-
-			return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/PNG")
-					.contentLength(videogame.get().getImageFile().length()).body(file);
-
-		} else {
-			return ResponseEntity.notFound().build();
-		}
-	}
-
-    @GetMapping("/showVideogameUser/{id}")
-    public String videogamePurchaseUser(Model model, HttpServletRequest request, @PathVariable long id) {
-        Principal principal = request.getUserPrincipal();
-
-        if (principal != null) {
-            return "redirect:/paymentConfirmation/"+ id;
-        } else {
-            return "redirect:/errorMessage";
-        }
-    } 
 
 }
