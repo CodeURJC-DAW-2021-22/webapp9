5ef9afecbf1906212c959f09dd0c2d8cfe006755
Xarlifue360 <xarlifue360@gmail.com>
2022-03-29 09:55:32 +0200

    me funciona dockers :3

--- backend/gamelink/src/main/java/urjc/gamelink/Controllers/LoginRestController.java
+++ backend/gamelink/src/main/java/urjc/gamelink/Controllers/LoginRestController.java
@@ -17,37 +17,33 @@ import urjc.gamelink.Configuration.Security.jwt.LoginRequest;
 import urjc.gamelink.Configuration.Security.jwt.UserLoginService;
 import urjc.gamelink.Configuration.Security.jwt.AuthResponse.Status;
 
-
-
+@RestController
+@RequestMapping("/api")
 public class LoginRestController {
-    
-    @RestController
-    @RequestMapping("/api")
-    public class LRestController {
-        
-        @Autowired
-        private UserLoginService userService;
-    
-        @PostMapping("/login")
-        public ResponseEntity<AuthResponse> login(
-                @CookieValue(name = "accessToken", required = false) String accessToken,
-                @CookieValue(name = "refreshToken", required = false) String refreshToken,
-                @RequestBody LoginRequest loginRequest) {
-            
-            return userService.login(loginRequest, accessToken, refreshToken);
-        }
-    
-        @PostMapping("/refresh")
-        public ResponseEntity<AuthResponse> refreshToken(
-                @CookieValue(name = "refreshToken", required = false) String refreshToken) {
-    
-            return userService.refresh(refreshToken);
-        }
-    
-        @PostMapping("/logout")
-        public ResponseEntity<AuthResponse> logOut(HttpServletRequest request, HttpServletResponse response) {
-    
-            return ResponseEntity.ok(new AuthResponse(Status.SUCCESS, userService.logout(request, response)));
-        }
+
+    @Autowired
+    private UserLoginService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @CookieValue(name = "accessToken", required = false) String accessToken,
+            @CookieValue(name = "refreshToken", required = false) String refreshToken,
+            @RequestBody LoginRequest loginRequest) {
+
+        return userService.login(loginRequest, accessToken, refreshToken);
     }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refreshToken(
+            @CookieValue(name = "refreshToken", required = false) String refreshToken) {
+
+        return userService.refresh(refreshToken);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<AuthResponse> logOut(HttpServletRequest request, HttpServletResponse response) {
+
+        return ResponseEntity.ok(new AuthResponse(Status.SUCCESS, userService.logout(request, response)));
+    }
+
 }
