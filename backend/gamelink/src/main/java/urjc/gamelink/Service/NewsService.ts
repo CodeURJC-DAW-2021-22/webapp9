236e98741deef73433dba07fb601aaa1377232e6
Xarlifue360 <xarlifue360@gmail.com>
2022-04-19 16:11:43 +0200

    intento de typescript

---
+++ backend/gamelink/src/main/java/urjc/gamelink/Service/NewsService.ts
@@ -0,0 +1,45 @@
+import { Service } from '@angular/core';
+import { HttpClient } fron '@angular/common/http'
+
+import { News } from '../Model/News';
+import { NewRepository } from '../Repositories/NewRepository';
+
+@Service
+export class NewsService {
+
+    constructor(private httpClient: HttpClient) {}
+
+    private repository: NewRepository;
+
+    findById(id: number) {
+        return this.repository.findById(id);
+    }
+
+    exist(id: number) {
+        return this.repository.existById(id);
+    }
+
+    findAll(pageable: Pageable) {
+        return this.repository.findAll(pageable);
+    }
+
+    findAll() {
+        return this.repository.findAll();
+    }
+
+    save(new: News) {
+        return this.httpClient.put(URL + new.id, new);
+    }
+
+    delete(id: number) {
+        this.repository.deleteById(id);
+    }
+
+    findAll(of: PageRequest) {
+        return this.repository.findAll(of);
+    }
+
+    findByIds(notices) {
+        return this.repository.findAllById(notices);
+    }
+}
