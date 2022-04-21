f7dac8290d9fd70c4c53e8bb3fef0d5ebc0a3e85
ultramani <71970337+ultramani@users.noreply.github.com>
2022-04-21 17:52:12 +0200

    Models done

---
+++ frontend/src/app/models/videojuego.model.ts
@@ -0,0 +1,26 @@
+export interface Videogame {
+    id?: number;
+    imageVg: boolean;
+    imageCompany: boolean;
+	title: string;
+    price:number;
+    rating:number;
+    genre:string;
+    company:string;
+    continent:string;
+    description:string;
+    shortDescription:string;
+    history:string;
+    
+    cpuR:string;
+    ramR:string;
+    ssooR:string;
+    gpuR:string;
+    storageR:string;
+    
+    cpuM:string;
+    ramM:string;
+    ssooM:string;
+    gpuM:string;
+    storageM:string;
+}
