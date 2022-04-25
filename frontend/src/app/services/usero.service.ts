389e38601aaf1f1e4b1767661a461e59192bd6b8
JhostinD <jhostin2906@gmail.com>
2022-04-25 12:35:09 +0200

    usero service v2

--- frontend/src/app/services/usero.service.ts
+++ frontend/src/app/services/usero.service.ts
@@ -15,23 +15,57 @@ export class SignupService {
 
     getUsers(): Observable<Usero[]> {
 		return this.http.get(BASE_URL).pipe(
-			//catchError(error => this.handleError(error)) da un error raro
+			catchError((error: any) => this.handleError(error))
 		) as Observable<Usero[]>;
 	}
 
     getUser(id: number | string): Observable<Usero> {
 		return this.http.get(BASE_URL + id).pipe(
-			//catchError(error => this.handleError(error))
+			catchError((error: any) => this.handleError(error))
 		) as Observable<Usero>;
 	}
 
-    createUser(user: Usero, password: String) {
+	createUser(user: Usero) {
+
+		if (!user.id) {
+			return this.http.post(BASE_URL, user)
+				.pipe(
+					catchError((error: any) => this.handleError(error))
+				);
+		} else {
+			return this.http.put(BASE_URL + user.id, user).pipe(
+				catchError((error: any) => this.handleError(error))
+			);
+		}
+	}
+
+	setUserImage(user: Usero, formData: FormData) {
+		return this.http.post(BASE_URL + user.id + '/image', formData)
+			.pipe(
+				catchError((error: any) => this.handleError(error))
+			);
+	}
+
+	deleteUserImage(user: Usero) {
+		return this.http.delete(BASE_URL + user.id + '/image')
+			.pipe(
+				catchError((error: any) => this.handleError(error))
+			);
+	}
+
+	updateUser(user: Usero) {
+		return this.http.put(BASE_URL + user.id, user).pipe(
+			catchError((error: any) => this.handleError(error))
+		);
+	}
+
+    /*createUser(user: Usero, password: String) {
         this.http.post(BASE_URL + '/signup', {username: user, password: password});
     }
 
     updateUser(user: Usero) {
         this.http.post(BASE_URL + '/updateUser', {});
-    }
+    }*/
 
     private handleError(error: any) {
 		console.log("ERROR:");
