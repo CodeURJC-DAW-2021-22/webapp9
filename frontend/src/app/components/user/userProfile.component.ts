10b3cb5409c806bd3c4e91ae43776a524dc6fb30
andrecupa <andrecupa@gmail.com>
2022-05-01 00:25:42 +0200

    user profile save no funciona

--- frontend/src/app/components/user/userProfile.component.ts
+++ frontend/src/app/components/user/userProfile.component.ts
@@ -22,7 +22,7 @@ export class UserProfile {
   constructor(private router: Router, activatedRoute: ActivatedRoute, public useroService: UseroService, public loginservice: LoginService) {
     const id = loginservice.currentUser()?.id;
     if (id) {
-      useroService.getUser(id).subscribe(
+      useroService.getMe().subscribe(
         (user) => this.user = user as Usero,
         (error: any) => console.error(error)
       );
@@ -43,27 +43,23 @@ export class UserProfile {
       (user: any) => this.uploadImage(user as Usero),
       (error: string) => alert('Error al guardar los datos: ' + error)
     );
-    this.useroService.getVideogames(this.user).subscribe(
-      videogame => this.videogame = videogame as Videogame[],
-      error => console.error(error)
-    );
   }
   cancel() {
     window.history.back();
   }
 
   uploadImage(user: Usero): void {
-    const image = this.file.nativeElement.files[0];
-    if (image) {
-      let formData = new FormData();
-      formData.append("imageFile", image);
-      this.useroService.setUserImage(user, formData).subscribe(
-        (_: any) => this.afterUploadImage(),
-        (error: string) => alert('Error al actualizar o subir la imagen: ' + error)
-      );
-    } else {
-      this.afterUploadImage();
-    }
+    // const image = this.file.nativeElement.files[0];
+    // if (image) {
+    //   let formData = new FormData();
+    //   formData.append("imageFile", image);
+    //   this.useroService.setUserImage(user, formData).subscribe(
+    //     (_: any) => this.afterUploadImage(),
+    //     (error: string) => alert('Error al actualizar o subir la imagen: ' + error)
+    //   );
+    // } else {
+    //   this.afterUploadImage();
+    // }
   }
 
   private afterUploadImage() {
