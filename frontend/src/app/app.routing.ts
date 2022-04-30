d4c8f66a14293a5e5bd7ec9762578960a7a38a88
Xarlifue360 <xarlifue360@gmail.com>
2022-04-30 18:12:59 +0200

    mini cambios

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,4 +1,5 @@
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
 
 import { HomeComponent } from './components/home/home.component';
 import { NewsComponent } from './components/news/news.component';
@@ -13,6 +14,7 @@ const appRoutes = [
     { path: 'news', component: NewsComponent },
     { path: 'showNews', component: ShowNewsComponent},
     { path: 'userProfile', component: UserProfile},
+    { path: 'admin', component: AdminComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full'}
 ]
 
